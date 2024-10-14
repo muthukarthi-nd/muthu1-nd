44a11f410808fc31cb247979993de29d51557f11
Muthukarthi-nd <karthiganexusdigitalia@gmail.com>
2024-10-14 13:06:30 +0530

    new changes added

--- route/transRoute.js
+++ route/transRoute.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const {getUser} = require('../controller/transaction');
 
-router.get('/userdetail', getUser);
+router.get('/v1', getUser);
 
 module.exports = router;
